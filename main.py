0dd5998b85e405992c0d542966b3e9e378175cd5
Eva Martinsone <mmmartinsone@gmail.com>
2021-09-24 03:15:23 +0300

    csv faila generesana

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from file_proc import pievienot, lasitRindinas
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -26,24 +26,31 @@ def postData():
     if request.method == 'GET':
         return redirect('/')
     elif request.method == 'POST':
-        print(request.form)
+       # izvelkam datus
         prieksmets = request.form.get('prieksmets')
-        # pievienot(vards)
+        atzime = request.form.get('atzime')
+        # izveidojam sarakstu
+        saraksts = list((prieksmets, atzime))
+        #pārbaudām sarakstu
+        print(saraksts)
+        # ar Pandas bibliotēku izveidojam csv datni, kur saglabāties
+        df = pd.DataFrame([saraksts])
+        df.to_csv('sekmes.csv', mode='a', index=False, header=False)
         return redirect('/sekmes?status=1')
     else:
         return "This method not supported!"
 
-@app.route('/lasitDatus')
-def lasitDatus():
-    rindinas = lasitRindinas()
-    dati = []
-    dati2 = []
-    for rindina in rindinas:
-        ieraksts = rindina.split(',')
-        print(rindina)
-        print(ieraksts)
-        dati.append(ieraksts)
-        dati2.append({'vards':ieraksts[0], 'uzvards':ieraksts[1], 'hobijs':ieraksts[2]})
+# @app.route('/lasitDatus')
+# def lasitDatus():
+#     rindinas = lasitRindinas()
+#     dati = []
+#     dati2 = []
+#     for rindina in rindinas:
+#         ieraksts = rindina.split(',')
+#         print(rindina)
+#         print(ieraksts)
+#         dati.append(ieraksts)
+#         dati2.append({'vards':ieraksts[0], 'uzvards':ieraksts[1], 'hobijs':ieraksts[2]})
 
     #print(dati)
     return render_template("dati.html", rindinas = dati, rindinas2 = dati2)
